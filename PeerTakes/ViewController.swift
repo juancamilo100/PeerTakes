15eeac45a8424e7a9279a2ca881577f5f2a2a31d
JuanEspinosa <juan.espinosa2@ge.com>
2016-06-12 22:39:48 -0400

    Created the project again

---
+++ PeerTakes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeerTakes
+//
+//  Created by Juan Espinosa on 6/12/16.
+//  Copyright © 2016 JuanEspinosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
