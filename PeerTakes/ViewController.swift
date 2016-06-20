9a309ac268ab1727c8192cf27a1d26ffa29d3c22
JuanEspinosa <juan.espinosa2@ge.com>
2016-06-20 13:33:41 -0400

    Minor change

--- PeerTakes/ViewController.swift
+++ PeerTakes/ViewController.swift
@@ -13,14 +13,14 @@ import JSON
 class ViewController: UIViewController {
     
     let googleApiKey = "AIzaSyDwM5YGbWpME6vHZ_RYf2QuxPoXZTS0P2s"
-    let googleApiRequestUrl = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDwM5YGbWpME6vHZ_RYf2QuxPoXZTS0P2s&channelId=UCKsvjO03BYgOaKcHNVsWz8Q&part=snippet,id&order=date&maxResults=20"
-    
     var videoLibrary = [Video]()
     
     @IBOutlet var playerView: YouTubePlayerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let googleApiRequestUrl = "https://www.googleapis.com/youtube/v3/search?key=\(self.googleApiKey)" + "&channelId=UCKsvjO03BYgOaKcHNVsWz8Q&part=snippet,id&order=date&maxResults=20"
 
         let videoDataFetcher = VideoDataFetcher(viewControllerInstance: self)
         videoDataFetcher.getVideoData(googleApiRequestUrl)
